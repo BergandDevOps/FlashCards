798e3e55dc3d9c477bdbf0cabd70c3f07ad56a17
Emilia Engberg <engberg.emilia@gmail.com>
2023-02-27 15:19:16 +0100

    Fix question

--- flashcards/src/FlashCard.jsx
+++ flashcards/src/FlashCard.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
 function FlashCard(props) {
-  const [isActive, setActive] = useState("false");
+  const [isActive, setActive] = useState(true);
   const ToggleClass = () => {
     setActive(!isActive);
   };
 
   return (
-    <div className="scene scene--card">
+    <div className='scene scene--card'>
       <div
         onClick={ToggleClass}
-        className='card {isActive ? "is-flipped" : "null"}'
+        className={`card ${isActive ? 'null' : 'is-flipped'}`}
       >
-        <div className="card__face card__face--front">front</div>
-        <div className="card__face card__face--back">back</div>
+        <div className='card__face card__face--front'>{props.question}</div>
+        <div className='card__face card__face--back'>{props.answer}</div>
       </div>
     </div>
   );
