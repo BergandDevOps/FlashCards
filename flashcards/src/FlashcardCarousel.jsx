bae20a56bcf241fed8f85610340c7cbd3581fcef
BergandDevOps <emiander92@gmail.com>
2023-02-27 16:42:51 +0100

    Passing onClickMethod to flashcard

--- flashcards/src/FlashcardCarousel.jsx
+++ flashcards/src/FlashcardCarousel.jsx
@@ -12,7 +12,7 @@ function FlashcardCarousel(props) {
     isVisibleCard[key] = isVisible
     return (
       <Carousel.Item key={key}>
-        <FlashCard question={question} answer={answer} isVisible={isVisible} />
+        <FlashCard question={question} answer={answer} onClickMethod={() => { setIsVisible(!isVisible)} } />
       </Carousel.Item>
     );
   }
