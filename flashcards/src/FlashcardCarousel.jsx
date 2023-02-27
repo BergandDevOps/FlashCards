52df79481bc7658bace9822991648ed55a1b6278
Alessandro Peretti <115103372+lsnprt@users.noreply.github.com>
2023-02-27 14:32:29 +0100

    Fixed the carousel, wrapped in a card element

--- flashcards/src/FlashcardCarousel.jsx
+++ flashcards/src/FlashcardCarousel.jsx
@@ -6,17 +6,21 @@ import Carousel from "react-bootstrap/Carousel";
 
 function FlashcardCarousel(props) {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <FlashCard question={"Question 1"} answer={"Answer 1"} />
-      </Carousel.Item>
-      <Carousel.Item>
-        <FlashCard />
-      </Carousel.Item>
-      <Carousel.Item>
-        <FlashCard />
-      </Carousel.Item>
-    </Carousel>
+    <Card style={{ width: "500px", backgroundColor: "grey" }}>
+      <Card.Body>
+        <Carousel>
+          <Carousel.Item>
+            <FlashCard question={"Question 1"} answer={"Answer 1"} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <FlashCard />
+          </Carousel.Item>
+          <Carousel.Item>
+            <FlashCard />
+          </Carousel.Item>
+        </Carousel>
+      </Card.Body>
+    </Card>
   );
 }
 export default FlashcardCarousel;
