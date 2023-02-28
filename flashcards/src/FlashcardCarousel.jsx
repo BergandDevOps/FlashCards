c7cbc3d6cdc131bdf5399536d7335e23c0e72c5f
BergandDevOps <emiander92@gmail.com>
2023-02-28 13:32:57 +0100

    made things work!

--- flashcards/src/FlashcardCarousel.jsx
+++ flashcards/src/FlashcardCarousel.jsx
@@ -1,47 +1,52 @@
-import FlashCard from './FlashCard';
-import Carousel from 'react-bootstrap/Carousel';
-import Card from 'react-bootstrap/Card';
-import { useState } from 'react';
+import FlashCard from "./FlashCard";
+import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
 function FlashcardCarousel(props) {
   let isVisibleCardArray = [];
   let setIsVisibleCardArray = [];
   let cards = MakeFlashcards(props.flashcardsData);
-  const [visibleCards, setVisibleCards] = useState(cards)
+  // const [visibleCards, setVisibleCards] = useState(cards);
 
-  const [activeCardIndex, setActiveCardIndex] = useState(0)
-  
+  const [activeCardIndex, setActiveCardIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
     setActiveCardIndex(selectedIndex);
   };
   function displayCards() {
-    
-    if (visibleCards.length > 0) {
+    if (cards.filter((c) => isVisibleCardArray[c.key]).length > 0) {
       //setVisibleCards(cards.filter((c) =>  isVisibleCardArray[c.key]))
       return (
-        <Carousel activeIndex={activeCardIndex} onSelect={handleSelect} interval={null} indicators={false}>
-          {cards.filter((c) => isVisibleCardArray[c.key] )}
+        <Carousel
+          activeIndex={activeCardIndex}
+          onSelect={handleSelect}
+          interval={null}
+          indicators={false}
+        >
+          {cards.filter((c) => isVisibleCardArray[c.key])}
           {/* {visibleCards} */}
-    </Carousel>);
-    
-  } else
+        </Carousel>
+      );
+    } else
       return (
-        <Carousel.Item>
-          <div className="scene scene--card">
-            <div className="card__face card__face--front">
-              Good work!
-              <button
-                onClick={() => {
-                  resetGame();
-                }}
-                className='btn btn-outline-light'
-              >
-                Reset
-              </button>
+        <Carousel>
+          <Carousel.Item>
+            <div className="scene scene--card">
+              <div className="card__face card__face--front">
+                Good work!
+                <button
+                  onClick={() => {
+                    resetGame();
+                  }}
+                  className="btn btn-outline-light"
+                >
+                  Reset
+                </button>
+              </div>
             </div>
-          </div>
-        </Carousel.Item>
+          </Carousel.Item>
+        </Carousel>
       );
   }
 
@@ -52,7 +57,7 @@ function FlashcardCarousel(props) {
     isVisibleCardArray[key] = isVisible;
     setIsVisibleCardArray[key] = setIsVisible;
     //make the flashcard
-    //removecard method uses setIsVisible of the current card to trigger change 
+    //removecard method uses setIsVisible of the current card to trigger change
     //in isVisibleCardArray which triggers a chagnge in visibleCards array used in the displayCards method
     // thereby triggering the rebuild of the carusel
     return (
@@ -61,9 +66,15 @@ function FlashcardCarousel(props) {
           question={question}
           answer={answer}
           removeCardMethod={() => {
-            setActiveCardIndex(0) //(activeCardIndex-1+visibleCards.length)%visibleCards.length
+            const len =
+              cards.filter((c) => isVisibleCardArray[c.key]).length - 1;
+            if (activeCardIndex === len) {
+              setActiveCardIndex((activeCardIndex - 1 + len) % len);
+            } else {
+              setActiveCardIndex((activeCardIndex + 1) % len);
+            }
             setIsVisible(false);
-            displayCards()
+            displayCards();
           }}
         />
       </Carousel.Item>
@@ -72,24 +83,22 @@ function FlashcardCarousel(props) {
 
   function MakeFlashcards(flashcardsData) {
     return flashcardsData.questions.map((f, i) =>
-      MakeFlashcard(f.question, f.answer, 'question' + i)
+      MakeFlashcard(f.question, f.answer, "question" + i)
     );
   }
-  
 
   function resetGame() {
-    for (let key in isVisibleCardArray) { 
-      setIsVisibleCardArray[key](true)
+    for (let key in isVisibleCardArray) {
+      setIsVisibleCardArray[key](true);
     }
-    
   }
 
   return (
     <div
-      className='text-center question-container'
-      style={{ width: '500px', backgroundColor: '#fdffcf' }}
+      className="text-center question-container"
+      style={{ width: "500px", backgroundColor: "#fdffcf" }}
     >
-      <h1 className='mt-3'>{props.flashcardsData.header}</h1>
+      <h1 className="mt-3">{props.flashcardsData.header}</h1>
       {displayCards()}
     </div>
   );
