f17eb26ff143a7fd5c19663028757228215c3a58
Emilia Engberg <engberg.emilia@gmail.com>
2023-02-28 13:15:22 +0100

    Add background color & padding

--- flashcards/src/FlashcardCarousel.jsx
+++ flashcards/src/FlashcardCarousel.jsx
@@ -7,29 +7,32 @@ function FlashcardCarousel(props) {
   let isVisibleCardArray = [];
   let setIsVisibleCardArray = [];
   let cards = MakeFlashcards(props.flashcardsData);
-  const [visibleCards, setVisibleCards] = useState(cards)
+  const [visibleCards, setVisibleCards] = useState(cards);
 
-  const [activeCardIndex, setActiveCardIndex] = useState(0)
-  
+  const [activeCardIndex, setActiveCardIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
     setActiveCardIndex(selectedIndex);
   };
   function displayCards() {
-    
     if (visibleCards.length > 0) {
       //setVisibleCards(cards.filter((c) =>  isVisibleCardArray[c.key]))
       return (
-        <Carousel activeIndex={activeCardIndex} onSelect={handleSelect} interval={null} indicators={false}>
-          {cards.filter((c) => isVisibleCardArray[c.key] )}
+        <Carousel
+          activeIndex={activeCardIndex}
+          onSelect={handleSelect}
+          interval={null}
+          indicators={false}
+        >
+          {cards.filter((c) => isVisibleCardArray[c.key])}
           {/* {visibleCards} */}
-    </Carousel>);
-    
-  } else
+        </Carousel>
+      );
+    } else
       return (
         <Carousel.Item>
-          <div className="scene scene--card">
-            <div className="card__face card__face--front">
+          <div className='scene scene--card'>
+            <div className='card__face card__face--front'>
               Good work!
               <button
                 onClick={() => {
@@ -52,7 +55,7 @@ function FlashcardCarousel(props) {
     isVisibleCardArray[key] = isVisible;
     setIsVisibleCardArray[key] = setIsVisible;
     //make the flashcard
-    //removecard method uses setIsVisible of the current card to trigger change 
+    //removecard method uses setIsVisible of the current card to trigger change
     //in isVisibleCardArray which triggers a chagnge in visibleCards array used in the displayCards method
     // thereby triggering the rebuild of the carusel
     return (
@@ -61,9 +64,9 @@ function FlashcardCarousel(props) {
           question={question}
           answer={answer}
           removeCardMethod={() => {
-            setActiveCardIndex(0) //(activeCardIndex-1+visibleCards.length)%visibleCards.length
+            setActiveCardIndex(0); //(activeCardIndex-1+visibleCards.length)%visibleCards.length
             setIsVisible(false);
-            displayCards()
+            displayCards();
           }}
         />
       </Carousel.Item>
@@ -75,20 +78,15 @@ function FlashcardCarousel(props) {
       MakeFlashcard(f.question, f.answer, 'question' + i)
     );
   }
-  
 
   function resetGame() {
-    for (let key in isVisibleCardArray) { 
-      setIsVisibleCardArray[key](true)
+    for (let key in isVisibleCardArray) {
+      setIsVisibleCardArray[key](true);
     }
-    
   }
 
   return (
-    <div
-      className='text-center question-container'
-      style={{ width: '500px', backgroundColor: '#fdffcf' }}
-    >
+    <div className='text-center question-container'>
       <h1 className='mt-3'>{props.flashcardsData.header}</h1>
       {displayCards()}
     </div>
